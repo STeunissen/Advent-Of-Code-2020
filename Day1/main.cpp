211ac15556c8ad667a569d6b26ecc662112c2a9a
STeunissen <rednasteun@msn.com>
2020-12-01 10:56:15 +0100

    Added part 2 of day 1

--- Day1/main.cpp
+++ Day1/main.cpp
@@ -24,12 +24,26 @@ void checkNumbers(std::vector<std::string> numbers){
         for (int j = 0; j < numbers.size(); ++j) {
             if ((std::stoi(numbers[i]) + std::stoi(numbers[j])) == 2020){
                 std::cout << numbers[i] + " + " + numbers[j] + " = " << std::stoi(numbers[i]) + std::stoi(numbers[j]) << std::endl;
+                std::cout << numbers[i] + " * " + numbers[j] + " = " << std::stoi(numbers[i]) * std::stoi(numbers[j]) << std::endl;
+            }
+        }
+    }
+}
+
+void checkNumbers2(std::vector<std::string> numbers){
+    for (int i = 0; i < numbers.size(); ++i) {
+        for (int j = 0; j < numbers.size(); ++j) {
+            for (int k = 0; k < numbers.size();++k) {
+                if ((std::stoi(numbers[i]) + std::stoi(numbers[j])) + std::stoi(numbers[k]) == 2020){
+                    std::cout << numbers[i] + " + " + numbers[j] + numbers[k] +" = " << std::stoi(numbers[i]) + std::stoi(numbers[j]) + std::stoi(numbers[k]) << std::endl;
+                    std::cout << numbers[i] + " * " + numbers[j] + " * " + numbers[k] +" = " << std::stoi(numbers[i]) * std::stoi(numbers[j]) * std::stoi(numbers[k]) << std::endl;
+                }
             }
         }
     }
 }
 
 int main() {
-    checkNumbers(readFile("E:\\Saxion\\Technische Informatica\\Datastructures\\Advent-Of-Code-2020\\Day1\\input.txt"));
+    checkNumbers2(readFile("E:\\Saxion\\Technische Informatica\\Datastructures\\Advent-Of-Code-2020\\Day1\\input.txt"));
     return 0;
 }
